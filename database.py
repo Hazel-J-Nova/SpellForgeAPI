ee248881b449884833c413ae43ec216c35cc9098
unknown <void.iros@gmail.com>
2022-03-20 10:45:46 -0500

    fixed env stuff

--- database.py
+++ database.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import bcrypt
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost:5432/spells'
+
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+load_dotenv()
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+password = os.getenv("POSTGRES_PASSWORD")
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{password}:1234@localhost:5432/spells'
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
